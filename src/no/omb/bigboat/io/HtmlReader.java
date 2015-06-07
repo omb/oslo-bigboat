10528e20ce96348f1f9cb6dbdfd7e96fb0ce517d
Olav <omb@esito.no>
2015-06-08 00:29:54 +0200

    Leser nå SailWave, Bundefjorden Rundt lagt til

--- src/no/omb/bigboat/io/HtmlReader.java
+++ src/no/omb/bigboat/io/HtmlReader.java
@@ -2,6 +2,8 @@ package no.omb.bigboat.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -10,8 +12,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import no.omb.bigboat.BigBoat;
+import no.omb.bigboat.Boat;
 import no.omb.bigboat.RaceData;
 import no.omb.bigboat.RaceEntry;
+import no.omb.bigboat.SeriesEntry;
 
 public class HtmlReader implements DataReader {
 
@@ -19,30 +23,57 @@ public class HtmlReader implements DataReader {
 
 	@Override
 	public List<RaceEntry> parseDataFile(RaceData race) {
-		List<String[]> entries = readDataFile(getFileName(race));
-		return parseEntries(entries);
+		return readDataFile(getFileName(race));
 	}
 
-	private List<String[]> readDataFile(String fileName) {
+	private List<RaceEntry> readDataFile(String fileName) {
 		File input = new File(fileName);
 		try {
 			Document doc = Jsoup.parse(input, BigBoat.CHARSET, "http://example.com/");
 			Elements tables = doc.select("table.racetable");
-			for (Element table : tables) {
-				Elements rows = table.select("tr.racerow");
-				for (Element row : rows) {
-					Elements cols = row.select("td");
-					System.out.println("Plass: " + cols.get(0) + " Båt: " + cols.get(2) + "-" + cols.get(3) + " Skipper: " + cols.get(6) + " Seilforening: " + cols.get(7));
-				}
-			}
+			return parseEntries(tables);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
-	private List<RaceEntry> parseEntries(List<String[]> entries) {
-		return null;
+	private List<RaceEntry> parseEntries(Elements tables) {
+		List<RaceEntry> raceEntries = new ArrayList<>();
+		for (Element table : tables) {
+			raceEntries.addAll(parseClassEntries(table));
+		}
+		Collections.sort(raceEntries);
+		return raceEntries;
+	}
+
+	private List<RaceEntry> parseClassEntries(Element table) {
+		List<RaceEntry> classEntries = new ArrayList<>();
+		Elements rows = table.select("tr.racerow");
+		int classSize = rows.size();
+		for (Element row : rows) {
+			Elements cols = row.select("td");
+			classEntries.add(parseEntry(cols, classSize));
+		}		
+		return classEntries;		
+	}
+
+	private RaceEntry parseEntry(Elements cols, int classSize) {
+		RaceEntry entry = new RaceEntry();
+		try {
+			entry.setPlaceNo(Integer.parseInt(cols.get(0).text()));
+			entry.setBoat(new Boat(cols.get(2).text() + "-" + cols.get(3).text(),
+					cols.get(5).text(),
+					cols.get(4).text(),
+					cols.get(6).text(),
+					cols.get(7).text()));
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Error in line: " + cols);
+		}
+		entry.setClassSize(classSize);
+		entry.setScore(entry.getPlaceNo() * SeriesEntry.SCORE_FACTOR * 1.0 / classSize);
+		return entry;
 	}
 
 	public static HtmlReader getInstance() {
